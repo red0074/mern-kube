c1068d7873104d05cdde58ff8c87767e80dcf3a8
red0074 <pdinesh122003@gmail.com>
2024-07-02 22:36:17 +0530

    updated the css and functionsalities to get perfect todo list

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Create() {
+  const [task, setTask] = useState("");
+  const handleAdd = () => {
+    axios
+      .post("http://localhost:3001/add", { task: task })
+      .then((result) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="create_form">
-      <input type="text" name="" id="" />
-      <button type="button">add</button>
+      <input
+        type="text"
+        placeholder="Enter the Task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        add
+      </button>
     </div>
   );
 }
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Create from "./Create";
-
+import axios from "axios";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import CheckBoxSharpIcon from "@mui/icons-material/CheckBoxSharp";
 function Home() {
   const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleEdit = (id) => {
+    axios
+      .put(`http://localhost:3001/update/${id}`)
+      .then((res) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:3001/delete/${id}`)
+      .then((res) => {
+        location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="home">
       <h2>todo list</h2>
@@ -12,7 +38,26 @@ function Home() {
           <h2>NO record</h2>
         </div>
       ) : (
-        todos.map((todo) => <div>{todo}</div>)
+        todos.map((todo) => (
+          <div className="task">
+            <div className="checkbox" onClick={() => handleEdit(todo._id)}>
+              {todo.done ? (
+                <CheckBoxSharpIcon className="icon" />
+              ) : (
+                <CheckBoxOutlineBlankIcon className="icon" />
+              )}
+              <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
+            </div>
+            <div>
+              <span>
+                <DeleteOutlineIcon
+                  className="icon"
+                  onClick={() => handleDelete(todo._id)}
+                />
+              </span>
+            </div>
+          </div>
+        ))
       )}
     </div>
   );
