7b575d27a2744cb5a97f2426deb17ee2a4a56a75
red0074 <pdinesh122003@gmail.com>
2024-07-05 10:56:23 +0530

    connection update and styling and dockerfile added

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-app");
+mongoose.connect("mongodb://mongo-service:27017/mern-app");
 
 app.get("/get", (req, res) => {
   TodoModel.find()
